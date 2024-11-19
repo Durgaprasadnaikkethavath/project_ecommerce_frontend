118d205d2c4a1d99edf36a5da40ea52d0bc217ad
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-19 22:16:36 +0530

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Navbar from "./Components/Navbar";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import ShopContextProvider from "./Context/ShopContext";
-import ImageCarousel from "./Components/ImageCarousel";
+// import ShopContextProvider from "./Context/ShopContext";
+// import ImageCarousel from "./Components/ImageCarousel";
 
 const App = () => {
   return (
     <>
+      <h1>React Vite</h1>
       <h1>Project Working</h1>
       <p>durga</p>
       <p>carousel Images</p>
       <p>prasad naik</p>
       <p>project e</p>
-      <BrowserRouter>
-        <ShopContextProvider>
-          <Navbar />
-          <ImageCarousel />
-        </ShopContextProvider>
-      </BrowserRouter>
+      <p>React Vite Project</p>
     </>
   );
 };
